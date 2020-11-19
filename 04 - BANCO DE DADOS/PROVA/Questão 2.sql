4cd3e286059641af49a03be888c9bc3df66c1775
ARIOCI CARLOS AMORIM <AAMORIM@LASALA.CORP>
2020-11-18 21:50:14 -0300

    prova_banco

--- 04 - BANCO DE DADOS/PROVA/Questão 2.sql
+++ 04 - BANCO DE DADOS/PROVA/Questão 2.sql
@@ -17,12 +17,12 @@ state       	char
 -- VALORES A SEREM INSERIDOS DENTRO DA TABELA PROVIDERS
 
 id	name                street	        city	        state	
-1	Henrique	        Av Brasil	    Rio de Janeiro	RJ	
-2	Marcelo Augusto	    Rua Imigrantes	Belo Horizonte	MG
-3	Caroline Silva	    Av São Paulo	Salvador	    BA	
-4	Guilerme Staff      Rua Central	    Porto Alegre	RS	
-5	Isabela Moraes	    Av Juiz Grande	Curitiba	    PR	
-6	Francisco Accerr	Av Paulista	São Paulo	        SP 	    	 
+(1,	'Henrique', 'Av Brasil', 'Rio de Janeiro',	'RJ')	
+(2,	'Marcelo Augusto', 'Rua Imigrantes',	'Belo Horizonte', 'MG')
+(3,	'Caroline Silva', 'Av São Paulo',	'Salvador', BA')	
+(4,	'Guilerme Staff', 'Rua Central',	'Porto Alegre' , 'RS')	
+(5,	'Isabela Moraes', 'Av Juiz Grande', 'Curitiba'  , 'PR')	
+(6,	'Francisco Accerr', 'Av Paulista', 'São Paulo' , 'SP')	    	 
 
 -- EXEMPLO DE SAÍDA ESPERADO
 city	
